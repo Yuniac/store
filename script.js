c08f7c4e27f1b2685cb799f151ae9e97abc18b09
Yusuf Sabbag <sist.heb@gmail.com>
2021-05-20 03:58:18 +0300

    search feature/cart feature

--- script.js
+++ script.js
@@ -1,97 +1,69 @@
 const log = console.log;
 
 const productsContainer = document.querySelector("#products");
-const cart = document.querySelector("#cart");
+const cartElement = document.querySelector("#cart");
 
 
-// item is the array of products from products.js
-function append(item) {
+// the populating function;
+function productElement(name, product, eventFunction) {
     let div = document.createElement("div");
     let h2 = document.createElement("h2");
     h2.classList.add("item-name")
-    h2.textContent = `${item.name}:`;
+    h2.textContent = name;
     let ul = document.createElement("ul");
-    for (let i = 1; i < 4; i++) {
-        if (i === 1) {
-            let li = document.createElement("li");
-            li.textContent = `${Object.keys(item)[i]}: ${item.category}`
-            ul.appendChild(li);
-        } else if (i === 2) {
-            let li = document.createElement("li");
-            li.textContent = `${Object.keys(item)[i]}: ${item.count}`
-            ul.appendChild(li);
-        } else {
-            let li = document.createElement("li");
-            li.textContent = `${Object.keys(item)[i]}: ${item.price}`
-            ul.appendChild(li);
-        }
-    }
+    ul.classList.add("items-details");
+    let listTexts = [
+        `category: ${product.category}`,
+        `count: ${product.count}`,
+        `price: ${product.price}`,
+    ]
+    listTexts.forEach(listText => {
+        let li = document.createElement("li");
+        li.textContent = listText;
+        ul.appendChild(li);
+    })
     div.appendChild(h2);
-    ul.classList.add("items-details")
     div.appendChild(ul);
     div.classList.add("product")
-    productsContainer.appendChild(div);
+
+    h2.addEventListener("click", eventFunction);
+
+    return div
 }
 
-function addToCart() {
-    const itemNames = Array.from(document.querySelectorAll(".item-name"));
-    itemNames.forEach(function(itemName) {
-        itemName.addEventListener("click", function() {
-            let productName = this.textContent;
-            let product = this.nextElementSibling;
-            // this will return the ul related to the item the user clicked;
-            // productDetails is the <li>s of that list, what we need to display;
-            let productDetails = product.childNodes;
-            let div = document.createElement("div");
-            let h2 = document.createElement("h2");
-            h2.classList.add("item-name-in-cart");
-            h2.textContent = productName;
-            let list = document.createElement("ul");
-            list.classList.add("items-details-in-cart")
-            productDetails.forEach(function(product) {
-                let itemName = product.innerText;
-                let cartItem = document.createElement("li");
-                // this will return the value but not the description;
-                // the breakpoint allows us to determine where the word "count" is and where is the actual count of items, its a sperator;
-                let breakPoint = itemName.indexOf(":") + 2;
-
-                if (itemName.startsWith("count")) {
-                    let countWord = itemName.slice(0, breakPoint);
-                    let count = itemName.slice(breakPoint);
-                    cartItem.textContent = count - (count - 1);
-
-                    function removeAnItem() {
-                        let itemsCount = countWord + (count - 1);
-                        // productDetails is the list of categ, count, price. We are getting the price one and working with it;
-                        productDetails[1].textContent = itemsCount;
-                    }
-                    // remove an item/reduce the count of items in stock by 1;
-                    removeAnItem();
-                } else {
-                    cartItem.textContent = itemName.slice(breakPoint);
-                }
-                cartItem.classList.add("item-in-cart");
-                list.appendChild(cartItem);
-            })
-            div.appendChild(h2)
-            div.appendChild(list);
-            div.classList.add("product-in-cart")
-                // this generates IDs for each cart item based on it's product name, we are going to need those so we don't get duplicates in the cart;
-            div.id = productName.slice(0, (productName.length - 1)).replace(/\s/g, "");
-            if (cart.querySelector(div.id) != null) {
-                div.count++;
-            } else {
-                cart.appendChild(div);
-            }
-        })
-    })
-
+// empties the cart element and repopulate the cart element accroding to the cart map;
+function removeProductFromCartEvent() {
+    // TODO: Remove from cart
 }
-list.forEach(item => {
-    append(item);
-})
 
-addToCart();
+function rebuildCartInDOM() {
+    cartElement.innerHTML = "";
+    nameToProductInCartMap.forEach((product, name) => {
+        cartElement.appendChild(
+            productElement(name, product, removeProductFromCartEvent)
+        );
+    });
+}
+
+function handleAddProductToCartEvent() {
+    let productName = this.textContent;
+    addToCart(productName);
+    rebuildCartInDOM();
+    rebuildProductsInDOM();
+}
+
+function rebuildProductsInDOM() {
+    productsContainer.innerHTML = "";
+    nameToProductInStockMap.forEach((product, name) => {
+        productsContainer.appendChild(
+            productElement(name, product, handleAddProductToCartEvent)
+        );
+    });
+}
+
+rebuildProductsInDOM();
+
+
 
 // the search bar;
 const search = document.querySelector("input");
@@ -104,17 +76,13 @@ search.addEventListener("keyup", () => {
         })
     }
     itemNames.forEach(item => {
-        if (item.textContent.includes(term) || item.textContent.startsWith(term) || item.textContent === term) {
+        if (item.textContent.includes(term)) {
             productsContainer.childNodes.forEach(product => {
                 if (product.firstChild.textContent.includes(term)) {
                     product.classList.add("show");
                 } else {
                     product.classList.add("hide");
                 }
-            })
-        } else {
-            productsContainer.childNodes.forEach(product => {
-                product.classList.add("hide");
             })
         }
     })
@@ -123,5 +91,3 @@ search.addEventListener("keyup", () => {
 window.onload = function() {
     search.value = "";
 }
-
-// if (item.textContent === term || item.textContent.includes(term) || item.textContent.startsWith(term))
