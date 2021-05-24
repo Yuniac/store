6223e099ddc59790f25b29edb146fd17c6855145
Yusuf Sabbag <sist.heb@gmail.com>
2021-05-24 15:06:08 +0300

    responsive implemented

--- script.js
+++ script.js
@@ -1,92 +1,158 @@
+const log = console.log;
+
 const productsContainer = document.querySelector("#products");
-const cart = document.querySelector("#cart");
+const searchElementdiv = document.querySelector("#search");
+const cartElementdiv = document.querySelector(".cart");
+const cartElement = document.querySelector("#cart");
 
 
-// item is the array of items from products.js
-function append(item) {
+// the populating function;
+function productElement(name, product, eventFunction) {
     let div = document.createElement("div");
     let h2 = document.createElement("h2");
     h2.classList.add("item-name")
-    h2.textContent = `${item.name}:`;
+    h2.textContent = name;
     let ul = document.createElement("ul");
-    for (let i = 1; i < 4; i++) {
-        if (i === 1) {
-            let li = document.createElement("li");
-            li.textContent = `${Object.keys(item)[i]}: ${item.category}`
-            ul.appendChild(li);
-        } else if (i === 2) {
-            let li = document.createElement("li");
-            li.textContent = `${Object.keys(item)[i]}: ${item.count}`
-            ul.appendChild(li);
-        } else {
-            let li = document.createElement("li");
-            li.textContent = `${Object.keys(item)[i]}:${item.price}`
-            ul.appendChild(li);
-        }
-    }
+    ul.classList.add("items-details");
+    let listTexts = [
+        `category: ${product.category}`,
+        `count: ${product.count}`,
+        `price: ${product.price}`,
+    ]
+    listTexts.forEach(listText => {
+        let li = document.createElement("li");
+        li.textContent = listText;
+        ul.appendChild(li);
+    })
     div.appendChild(h2);
-    ul.classList.add("items-details")
     div.appendChild(ul);
     div.classList.add("product")
-    productsContainer.appendChild(div);
+    h2.addEventListener("click", eventFunction);
+    return div
 }
 
-function addToCart() {
+// empties the cart element and repopulate the cart element accroding to the cart map;
+function rebuildCartInDOM() {
+    cartElement.innerHTML = "";
+    nameToProductInCartMap.forEach((product, name) => {
+        cartElement.appendChild(
+            productElement(name, product, removeProductFromCartEvent)
+        );
+    });
+}
+
+function handleAddProductToCartEvent() {
+    let productName = this.textContent;
+    addToCart(productName);
+    rebuildCartInDOM();
+    rebuildProductsInDOM();
+}
+
+function rebuildProductsInDOM() {
+    productsContainer.innerHTML = "";
+    nameToProductInStockMap.forEach((product, name) => {
+        productsContainer.appendChild(
+            productElement(name, product, handleAddProductToCartEvent)
+        );
+    });
+}
+nameToProductInCartMap = new Map();
+
+function addToCart(name) {
+    let productInStock = nameToProductInStockMap.get(name);
+    if (nameToProductInCartMap.has(name)) {
+        // Add to existing product.
+        let updatedCount = nameToProductInCartMap.get(name).count + 1
+        nameToProductInCartMap.set(name, {
+            category: productInStock.category,
+            count: updatedCount,
+            price: updatedCount * productInStock.price,
+        });
+    } else {
+        // Add new product.
+        nameToProductInCartMap.set(name, {
+            category: productInStock.category,
+            count: 1,
+            price: productInStock.price,
+        });
+    }
+    productInStock.count--;
+}
+rebuildProductsInDOM();
+// Remove from cart;
+function removeProductFromCartEvent(name) {
+    if (nameToProductInCartMap.has(name)) {
+        let productInCart = nameToProductInCartMap.get(name);
+        if (productInCart.count > 0) {
+            productInCart.count--;
+            console.log(productInCart.count);
+        } else {
+            console.log("minus")
+        }
+    } else {
+        console.log(nameToProductInCartMap);
+    }
+}
+
+
+
+// the search bar;
+const search = document.querySelector("input");
+search.addEventListener("keyup", () => {
+    let term = search.value;
     const itemNames = Array.from(document.querySelectorAll(".item-name"));
-    itemNames.forEach(function(itemName) {
-        itemName.addEventListener("click", function() {
-            let productName = this.textContent;
-            let product = this.nextElementSibling;
-            // this will return the ul related to the item the user clicked;
-            // productDetails is the <li>s of that list, what we need to display;
-            let productDetails = product.childNodes;
-            let div = document.createElement("div");
-            let h2 = document.createElement("h2");
-            h2.classList.add("item-name-in-cart");
-            h2.textContent = productName;
-            let list = document.createElement("ul");
-            list.classList.add("items-details-in-cart")
-            productDetails.forEach(function(product) {
-                let itemName = product.innerText;
-                let cartItem = document.createElement("li");
-                // this will return the value but not the description;
-                // the breakpoint allows us to determine where the word "count" is and where is the actual count of items, its a sperator;
-                let breakPoint = itemName.indexOf(":") + 2;
-                if (itemName.startsWith("count")) {
-                    let countWord = itemName.slice(0, breakPoint);
-                    let count = itemName.slice(breakPoint);
-                    cartItem.textContent = count - (count - 1);
-
-                    function removeAnItem() {
-                        let itemsCount = countWord + (count - 1);
-                        // productDetails is the list of cat, count, price. We are getting the price one and working with it;
-                        productDetails[1].textContent = itemsCount;
-
-                    }
-                    removeAnItem();
-                } else {
-                    cartItem.textContent = itemName.slice(breakPoint);
-                }
-                cartItem.classList.add("item-in-cart");
-                list.appendChild(cartItem);
-            })
-            div.appendChild(h2)
-            div.appendChild(list);
-            div.classList.add("product-in-cart")
-            cart.appendChild(div);
-            cart.childNodes.forEach(item => {
-                console.log(item.id);
-            })
-
+    if (term === "") {
+        productsContainer.childNodes.forEach(product => {
+            product.classList.remove("hide");
         })
+    }
+    itemNames.forEach(item => {
+        if (item.textContent.includes(term)) {
+            productsContainer.childNodes.forEach(product => {
+                if (product.firstChild.textContent.includes(term)) {
+                    product.classList.add("show");
+                } else {
+                    product.classList.add("hide");
+                }
+            })
+        }
     })
-
-}
-list.forEach(item => {
-    append(item);
 })
 
-// remove items from in-stock;
+window.onload = function() {
+    search.value = "";
+}
 
-// item here refers to the item that got added to the cart, where this function is called // the paramter passed to it is the "this" keyword;
-addToCart();
+
+// responsiveness functions;
+
+const searchButton = document.querySelector("#search-button");
+const cartButton = document.querySelector("#cart-button");
+
+// function showSideMenu(div) {
+//     div.style.marginInline = 0;
+// }
+function showSideMenu(div) {
+    if (div.classList.contains("toggle-side-menu")) {
+        div.classList.remove("toggle-side-menu");
+    } else {
+        div.classList.add("toggle-side-menu");
+    }
+}
+
+// function showSideMenu(div) {
+//     if (div.style["marginInline"] === "-25rem") {
+//         div.style["marginInline"] = 0;
+//         if (div.classList.contains("cart")) {
+//             div.style.right = 0;
+//         }
+//     } else {
+//         div.style["marginInline"] = "-25rem";
+//     }
+// }
+searchButton.addEventListener("click", () => {
+    showSideMenu(searchElementdiv);
+})
+cartButton.addEventListener("click", () => {
+    showSideMenu(cartElementdiv);
+})
